01e44d806daf82c76b3fe5d373d994166dfe2a0e
afarag11 <afarag11amr.fa91@gmail.com>
2019-12-12 11:37:37 -0500

    done

---
+++ src/main/java/com/example/demo/Boot302Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot302Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot302Application.class, args);
+    }
+
+}
